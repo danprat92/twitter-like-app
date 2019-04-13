c8fb0eddc15bd82429966355df4bc9cbb9ccf42d
Daniel Prato <dprato@grio.com>
2019-04-13 14:43:19 -0500

    Removing stuff old project

--- src/messages.ts
+++ src/messages.ts
@@ -1,9 +1,5 @@
 const messages = {
-  INTRO: 'Hello, I\'m ',
-  NAME: 'Daniel Prato',
-  ROLE: 'I\'m a full-stack web and multiplatform mobile developer',
-  WIP: 'This is a WIP, i will be updating it with much more information and features soon.',
-  CONTACT_ME: 'In the meantime, feel free to get in touch with me using my social networks',
+  INTRO: 'Twitter app WIP',
 };
 
 export default messages;
