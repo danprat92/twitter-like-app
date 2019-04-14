d338211f5e64e5b5a5a1bcf07f227dc5e01f2ebc
Daniel Prato <dprato@grio.com>
2019-04-14 16:11:14 -0500

    Fixing unit test

--- src/containers/AppContainer/App.test.tsx
+++ src/containers/AppContainer/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './index';
+import { Provider } from 'react-redux';
+import configureStore from '../../configureStore';
+
+const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AppContainer />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
