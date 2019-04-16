1428c8ba37507a34df177e9fb3cab62dea316b6f
Daniel Prato <dprato@grio.com>
2019-04-16 14:11:48 -0500

    Env config

--- fill-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-touch .env
-echo 'REACT_APP_FIREBASE_API_KEY="$REACT_APP_FIREBASE_API"' >> .env
-echo 'REACT_APP_AUTH_URL="$REACT_APP_AUTH_URL"' >> .env
-echo 'REACT_APP_DATABASE_URL="$REACT_APP_FIREBASE_API"' >> .env
-
-less -FX .env
